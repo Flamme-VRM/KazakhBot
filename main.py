e2900380d3b994e3a0289c84d4a29949c87ec2d1
Flamme <shyngisbekovasylkhan@gmail.com>
2025-06-11 00:43:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
+genai.configure(api_key=GEMINI_API_KEY)
+model = genai.GenerativeModel('gemini-2.5-flash-preview-05-20')
+
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
