f5f22c10e36f9e570f89da027ad0f93097febd29
Flamme <shyngisbekovasylkhan@gmail.com>
2025-06-13 02:59:12 +0500

    Update main.py
    moved the model into .env file to enhance the code architecture

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel('gemini-2.5-flash-preview-05-20')
+model = genai.GenerativeModel(os.getenv("MODEL")
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
