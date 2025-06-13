0818fff8b4ea6e860e16d896635c983400c38e36
Flamme <shyngisbekovasylkhan@gmail.com>
2025-06-13 08:17:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
+logger = logging.getLogger(__name__)
 
 load_dotenv(".env")
 
